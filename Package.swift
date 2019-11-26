acd85997286666694b0070c2eab7a3f673e078fb
Tomohiro Imaizumi <info@imaizu.me>
2019-11-27 01:50:55 +0900

    swiftlintを導入

--- Package.swift
+++ Package.swift
@@ -14,6 +14,6 @@ let package = Package(
             dependencies: ["Html"]),
         .testTarget(
             name: "imaizume.github.ioTests",
-            dependencies: ["imaizume.github.io"]),
+            dependencies: ["imaizume.github.io"])
     ]
 )
--- Tests/imaizume.github.ioTests/XCTestManifests.swift
+++ Tests/imaizume.github.ioTests/XCTestManifests.swift
@@ -3,7 +3,7 @@ import XCTest
 #if !canImport(ObjectiveC)
 public func allTests() -> [XCTestCaseEntry] {
     return [
-        testCase(imaizume_github_ioTests.allTests),
+        testCase(imaizume_github_ioTests.allTests)
     ]
 }
 #endif
