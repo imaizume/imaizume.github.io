fb01ae95b30ff3417099edf4069560038d396094
Tomohiro Imaizumi <bonriki.life@gmail.com>
2022-05-14 13:10:51 +0900

    Update packages

--- Package.swift
+++ Package.swift
@@ -5,13 +5,16 @@ import PackageDescription
 let package = Package(
     name: "imaizume.github.io",
     dependencies: [
-        .package(url: "https://github.com/pointfreeco/swift-html.git", from: "0.3.0")
-
+        .package(url: "https://github.com/pointfreeco/swift-html.git", from: "0.3.0"),
+                .package(url: "https://github.com/malcommac/SwiftDate.git", from: "6.3.0"),
     ],
     targets: [
         .target(
             name: "imaizume.github.io",
-            dependencies: ["Html"]),
+            dependencies: [
+                "Html",
+                "SwiftDate",
+            ]),
         .testTarget(
             name: "imaizume.github.ioTests",
             dependencies: ["imaizume.github.io"])
